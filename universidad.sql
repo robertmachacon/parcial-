567f271d91cd55f6f2192f9254c952bdbafc8ba3
robertmachacon <99507218+robertmachacon@users.noreply.github.com>
2022-05-13 16:15:01 -0500

    Actividad en clase de cursores

---
+++ universidad.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 13-05-2022 a las 21:09:54
+-- Versión del servidor: 8.0.17
+-- Versión de PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `universidad`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuario` (IN `nid` INT(11))  BEGIN
+
+  DECLARE listo boolean DEFAULT false;
+  DECLARE v_id int;
+  DECLARE v_estado VARCHAR(120);  
+  
+DECLARE universidad CURSOR for SELECT id, estado 
+                                FROM usuario;
+DECLARE CONTINUE HANDLER for SQLSTATE '02000' SET listo = true;
+OPEN universidad;
+universidadLoop:LOOP
+FETCH universidad INTO v_id, v_estado;
+  if listo THEN LEAVE universidadLoop;
+  END if;
+  UPDATE usuario set estado = 'inactivo' WHERE id = v_id and id =nid;
+END Loop universidadLoop;
+CLOSE universidad;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `identificacion` int(11) NOT NULL,
+  `Rol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nombre`, `estado`, `identificacion`, `Rol`) VALUES
+(1, 'Valentina', 'inactivo', 123456, 'Estudiante '),
+(2, 'Lobelsito', 'inactivo', 98765, 'Profesor ');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
